4481f1b2622d60aeac1311f9652a7f4721ce5eed
german adrian anguiano perez <adrian2009@live.com.mx>
2018-08-23 08:39:39 -0700

    Imprimos las materias

--- Colecciones/Program.cs
+++ Colecciones/Program.cs
@@ -10,12 +10,50 @@ namespace Colecciones
     {
         static void Main(string[] args)
         {
+            //DECLARACION E INICIALIZACION DE LISTA DE ALUMNOS
             List<Alumno> alumnos = new List<Alumno>();
-            alumnos.Nombre.Add(new Alumno());
+            //asigancion
+            Alumno alumno1 = new Alumno();
+            alumno1.Nombre = "Jose Perez";
+            alumno1.Materias.Add(new Materia("Historia", "HST741"));
+            alumno1.Materias.Add(new Materia("Matematicas", "MAT741"));
+            alumno1.Materias.Add(new Materia("Civismo", "CVC753"));
+           
          
 
+            Alumno alumno2 = new Alumno();
+            alumno2.Nombre = "Arturo  Lopez";
+            alumno2.Materias.Add(new Materia("Español", "ESP637"));
+            alumno2.Materias.Add(new Materia("Quimica", "QUI285"));
+            alumno2.Materias.Add(new Materia("Programacion", "PROG756"));
 
-            Materia matematicas = new Materia();
+            Alumno alumno3 = new Alumno();
+            alumno3.Nombre = "Juan Hernandez";
+            alumno3.Materias.Add(new Materia("Fisica", "FIS052"));
+            alumno3.Materias.Add(new Materia("Matematicas", "MAT391"));
+            alumno3.Materias.Add(new Materia("Dibujo", "DIB891"));
+
+            Alumno alumno4 = new Alumno();
+            alumno4.Nombre = "Maria  Gutierrez";
+            alumno4.Materias.Add(new Materia("Dibujo", "DIB891"));
+            alumno4.Materias.Add(new Materia("Quimica", "QUI285"));
+            alumno4.Materias.Add(new Materia("Civismo", "CVC753"));
+
+            alumnos.Add(alumno1);
+            alumnos.Add(alumno2);
+            alumnos.Add(alumno3);
+            alumnos.Add(alumno4);
+
+            foreach (Alumno elemento in alumnos)
+            {
+                Console.WriteLine("Nombre: " + elemento.Nombre);
+                foreach(Materia materia in elemento.Materias)
+                {
+                    Console.WriteLine("Materia:" + materia.Nombre);
+                }
+            }
+
+            /*Materia matematicas = new Materia();
             matematicas.Nombre = "Matematicas";
             matematicas.Identificador = "MT1258";
 
@@ -41,7 +79,7 @@ namespace Colecciones
             {
                 Console.WriteLine("Materia: " + materia.Nombre);
                 
-            }
+            }*/
             Console.ReadLine();
         }
     }
